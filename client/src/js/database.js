24a21d5602b6ab925d92a35b520680d529397e26
Tony Trinh <tonytrinh911@gmail.com>
2023-05-24 18:32:01 -0400

    updated putDB method in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,18 +16,19 @@ const initdb = async () =>
 export const putDb = async (content) => {
   console.error("Put in database");
   const jateDB = await openDB("jate", 1);
-  const transaction = jateDB.transaction("jate", "readwrite");
-  const store = transaction.objectStore("jate");
-  await store.add(content);
-  await transaction.done;
+  const tx = jateDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({id:1, value: content});
+  const result = await request;
+  console.log("Content successfully put into database", result.value);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.error("Get from database");
   const jateDB = await openDB("jate", 1);
-  const transaction = jateDB.transaction("jate", "readonly");
-  const store = transaction.objectStore("jate");
+  const tx = jateDB.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
   const content = await store.getAll();
   await transaction.done;
   return content;
