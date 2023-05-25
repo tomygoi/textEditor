16321180f7e1ac3056c1aeee1dd253896b5839b3
Tony Trinh <tonytrinh911@gmail.com>
2023-05-25 16:03:08 -0400

    configured plugins for webpack, added CSS loaders and babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }),
+
+      new InjectManifest({
+        swSrc: '.src-sw.js',
+        swDest: 'src-sw.js',
+      }), 
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true, 
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.', 
+        description: 'Take notes with Javascript syntax highlighting!',
+        theme_color: '#225ca3',
+        background_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'), 
+          },
+        ],
+      }),
       
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/, 
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+
         
       ],
     },
